031375545c8cc2122d43b9498e4acb181836fccf
Simon Nordlund Magnusson <Simon.Nordlund.Magnusson@gmail.com>
2026-09-21 13:15:36 +0200

    tamagotchi

---
+++ TamaGotchi/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+Tamagotchi pet = new Tamagotchi();
+while (true)
+{
+    Console.WriteLine("What would you like to name your gochi");
+    pet.start();
+
+    while (true)
+    {  
+        Console.WriteLine("What would you like to do");
+        Console.WriteLine("""
+        1. Printstats:
+        2. Feed:
+        3. Hi:
+        4. Do nothing: 
+        5. Teach:
+        """);
+        string choice = Console.ReadLine();
+        int choiceInt;
+        while (!int.TryParse(choice, out choiceInt)|| choiceInt < 1 && choiceInt > 5)
+        {
+        Console.WriteLine("What would you like to do");
+        Console.WriteLine("""
+        1. Printstats:
+        2. Feed:
+        3. Hi:
+        4. Do nothing: 
+        5. Teach:
+        """);
+        choice = Console.ReadLine();
+        }
+        if (choiceInt == 1)
+        {
+            pet.PrintStats();
+        }
+        if (choiceInt == 2)
+        {
+            pet.Feed();
+        }
+        if (choiceInt == 3)
+        {
+            pet.Hi();
+        }
+        if (choiceInt == 4)
+        {
+            pet.Tick();
+        }
+        if (choiceInt == 5)
+        {
+            Console.WriteLine($"What would you like to teach {pet._name}");
+            pet.Teach(Console.ReadLine());
+        }
+        
+        Console.WriteLine("Press space to continue");
+        while (true)
+        {
+            if(Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+            {
+                break;
+            }
+        }
+        if (pet.GetAlive() == false)
+        {
+            Console.WriteLine("Your");
+            break;
+        }
+    
+    }
+}
+
---
+++ TamaGotchi/Tamagotchi.cs
@@ -0,0 +1,83 @@
+
+public class Tamagotchi
+{
+    private int _hunger;
+    private int _boredom;
+    private List<string> _words = ["Hi"];
+    private bool _isAlive;
+    public string _name;
+
+
+    public void start()
+    {
+        _hunger = 0;
+        _boredom = 0;
+        _words = ["Hi"];
+        _isAlive = true;
+        _name = Console.ReadLine();
+    }
+
+
+    public void Feed()
+    {
+        _hunger -=4;
+        
+        Tick();
+        if (_hunger < 0)
+        {
+            _hunger = 0;
+        }
+    }
+    public void Hi()
+    {
+        int x = Random.Shared.Next(0, _words.Count);
+        string word = _words[x];
+        Console.WriteLine(word);
+        ReduceBoredom();
+        Tick();
+    }
+    public void Teach(string word)
+    {
+        
+        _words.Add(word);
+        ReduceBoredom();
+        Tick();
+        
+        
+    }
+    public void Tick()
+    {
+        _hunger ++;
+        _boredom ++;
+        if(_hunger > 10|| _boredom > 10)
+        {
+            _isAlive = false;
+
+        }
+    }
+    public void PrintStats()
+    {
+        Console.WriteLine($"""
+             info;
+        hunger:  {_hunger}
+        Boredom: {_boredom}
+        Alive:   {GetAlive()}
+        """);
+        Tick();
+    }
+    public bool GetAlive()
+    {
+        
+        return _isAlive;
+    }
+    private void ReduceBoredom()
+    {
+        _boredom -=4;
+        if (_boredom < 0)
+        {
+            _boredom = 0;
+        }
+    }
+    
+
+} 
